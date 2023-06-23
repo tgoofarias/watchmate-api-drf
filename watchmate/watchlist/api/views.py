5a9b3a9118e71770eb5e1c2d2d59550afbd44a37
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2023-06-22 21:13:36 -0400

    Movie update and delete

--- watchmate/watchlist/api/views.py
+++ watchmate/watchlist/api/views.py
@@ -4,14 +4,32 @@ from rest_framework.decorators import api_view
 from watchlist.models import Movie
 from .serializers import MovieSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(instance=movies, many=True)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(instance=movies, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        return Response(data=serializer.errors)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(id=pk)
-    serializer = MovieSerializer(instance=movie, many=False)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        movie = Movie.objects.get(id=pk)
+        serializer = MovieSerializer(instance=movie, many=False)
+        return Response(data=serializer.data)
+    elif request.method == 'PUT':
+        movie = Movie.objects.get(id=pk)
+        serializer = MovieSerializer(instance=movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        return Response(data=serializer.errors)
+    elif request.method == 'DELETE':
+        movie = Movie.objects.get(id=pk)
+        movie.delete()
